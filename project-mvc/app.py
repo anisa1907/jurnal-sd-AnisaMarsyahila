cc64dc51c122983447409dd4e58cb1928e992a41
anisa1907 <marsyaanisa00@gmail.com>
2026-06-25 13:22:52 +0700

    [FEAT] Menambahkan komponen dashboard dan state management sederhana

--- project-mvc/app.py
+++ project-mvc/app.py
@@ -1,2 +1,19 @@
-if __name__ == "__main__": 
-    run_app() 
+from views.dashboard_component import show_dashboard
+
+state = {
+    "user": "Admin",
+    "score": 0
+}
+
+
+def tambah_score():
+    state["score"] += 10
+
+
+print("State awal:")
+show_dashboard(state["user"], state["score"])
+
+tambah_score()
+
+print("\nState setelah update:")
+show_dashboard(state["user"], state["score"])
---
+++ project-mvc/views/dashboard_component.py
@@ -0,0 +1,5 @@
+def show_dashboard(user, score):
+    print("\n===== DASHBOARD =====")
+    print(f"User  : {user}")
+    print(f"Score : {score}")
+    print("=====================")
